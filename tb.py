635a31852e36a8571433770b269ce6f48d199f01
just-another-user <thisismyregularmail@gmail.com>
2022-04-16 15:06:52 +0300

    wip

--- tb.py
+++ tb.py
@@ -8,7 +8,6 @@ from db import DatabaseSqlite3
 
 
 class TwitterBot:
-	default_creds_filename = ".creds"
 	search_url = "https://api.twitter.com/2/tweets/search/recent"
 	default_db_filename = "tb.db"
 	url_to_tweet = "https://twitter.com/i/web/status"
@@ -21,29 +20,30 @@ class TwitterBot:
 	);"""
 	url_regex = re.compile(r"\bhttps?://t\.co/\S+")
 
-	def __init__(self, creds_filename=None):
-		self.creds_filename = creds_filename or self.default_creds_filename
-		self.bearer_token = self._get_creds()
-		self.db = DatabaseSqlite3(self.default_db_filename)
-		self._init_db()
-
-	def _init_db(self):
+	def __init__(self, token, db_filename=None):
+		"""
+		:param str token: The Twitter bot's bearer token.
+		:param str db_filename: (optional) The name of the database file to save tweets to.
+		"""
+		self.bearer_token = f"Bearer {token}"
+		self.db = DatabaseSqlite3(db_filename or self.default_db_filename)
 		table_created = self.db.run_query(self._tb_schema)
 		if not table_created["success"]:
 			raise Exception(table_created["error"])
 
-	def _get_creds(self):
-		with open(self.creds_filename, encoding="utf-8") as f:
-			bearer_token = f"Bearer {f.read().strip()}"
-		return bearer_token
+	def get_tweets_for_phrase(self, search_phrase, max_results):
+		"""
 
-	def get_tweets_by_keyword(self, keyword, max_results):
+		:param str search_phrase: The phrase to search for.
+		:param int max_results: The maximum number of retrieved results. Must be between 10 and 100.
+		:return dict: Success=True and results if successful; success=False and error message otherwise.
+		"""
 		result = {"success": False}
 		if not 100 >= max_results >= 10:
 			result["error"] = f"The results limit must be between 10 and 100. Got {max_results}"
 		else:
 			try:
-				params = {"query": f"{keyword} -is:retweet", "max_results": max_results}
+				params = {"query": f"{search_phrase} -is:retweet", "max_results": max_results}		# Skip retweets.
 				headers = {"Authorization": self.bearer_token}
 				resp = requests.get(self.search_url, headers=headers, params=params)
 				if resp.status_code == 200:
@@ -56,9 +56,19 @@ class TwitterBot:
 		return result
 
 	def _parse_tweet(self, text):
+		"""
+		Remove Twitter shortened URLs as many links to the same source are given different addresses, making the
+		Tweet appear different even though the content is the same.
+		:param str text: The tweet to parse.
+		:return str: The parsed tweet.
+		"""
 		return self.url_regex.sub("", text).strip()
 
 	def _exists_in_db_in_either_text_or_id(self, tweet_details):
+		"""
+		:param dict tweet_details: The tweet id and its parsed text.
+		:return Boolean: True if the tweet was seen before; False otherwise.
+		"""
 		query = """
 		SELECT * FROM seen_tweets
 		WHERE
@@ -84,7 +94,7 @@ class TwitterBot:
 	def get_latest_tweets_by_keywords(self, keywords, max_results):
 		tbk = {}
 		for keyword in keywords:
-			res = self.get_tweets_by_keyword(keyword, max_results)
+			res = self.get_tweets_for_phrase(keyword, max_results)
 			if not res["success"]:
 				raise Exception(res["error"])
 			new_tweets = []
@@ -100,12 +110,13 @@ if __name__ == '__main__':
 	import json
 	from argparse import ArgumentParser
 	parser = ArgumentParser(description="Twitter Bot")
+	parser.add_argument("token", action="store", type=str, help="Bearer token for Twitter.")
 	parser.add_argument("keywords", metavar="KEYWORD", action="extend", nargs="+", type=str,
 						help="Keyword(s) to retrieve results for")
 	parser.add_argument("-l", "--limit", action="store", type=int, default=10,
 						help="Max number of tweets to retrieve per keyword")
 	args = parser.parse_args()
 
-	tb = TwitterBot()
+	tb = TwitterBot(args.token)
 	tbks = tb.get_latest_tweets_by_keywords(args.keywords, args.limit)
 	print(json.dumps(tbks, indent=2))
